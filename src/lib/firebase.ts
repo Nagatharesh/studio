e9bd4f05a7efa807720e144cad8aa7a37f16b092
Naresh Kotti <nareshkottees@gmail.com>
2025-09-07 02:56:25 +0000

    <script type="module">   // Import the functions you need from the SDKs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "agrichain-r37ux",
-  "appId": "1:281129452440:web:074a9e59d3708cbfe0e1ed",
-  "storageBucket": "agrichain-r37ux.firebasestorage.app",
-  "apiKey": "AIzaSyCt7OZ4_9wKDdISssdLoipl2ovsZkF5kX4",
-  "authDomain": "agrichain-r37ux.firebaseapp.com",
-  "messagingSenderId": "281129452440"
-};
+    apiKey: "AIzaSyCt7OZ4_9wKDdISssdLoipl2ovsZkF5kX4",
+    authDomain: "agrichain-r37ux.firebaseapp.com",
+    projectId: "agrichain-r37ux",
+    storageBucket: "agrichain-r37ux.firebasestorage.app",
+    messagingSenderId: "281129452440",
+    appId: "1:281129452440:web:fc83b0428dea707de0e1ed"
+  };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
